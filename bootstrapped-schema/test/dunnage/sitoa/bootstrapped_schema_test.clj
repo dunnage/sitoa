c0b394fac8abea433305b5d0b0952eb2cc5cd647
Tyler Tallman <dspiteself@gmail.com>
2022-02-24 16:46:40 -0600

    fix serialization

--- bootstrapped-schema/test/dunnage/sitoa/bootstrapped_schema_test.clj
+++ bootstrapped-schema/test/dunnage/sitoa/bootstrapped_schema_test.clj
@@ -8,15 +8,9 @@
             [malli.generator :as mg]))
 
 (comment
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/ecl.xsd"))
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/datatypes.xsd"))
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/script.xsd"))
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/specialized.xsd"))
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/structures.xsd"))
-  (xds->registry {:default-ns "script"} (io/resource "V2021011SCRIPT/transport.xsd"))
-  (xds->registry {:default-ns "xsd"} (io/resource "XMLSchema.xsd"))
-  (xds->registry {:default-ns "fop"} (io/resource "fop.xsd"))
-  (xsd->schema  {:default-ns "script"} (io/resource "V2021011SCRIPT/transport.xsd"))
+  (xsd->schema {:default-ns "xsd"} (io/resource "XMLSchema.xsd"))
+  (xsd->schema {:default-ns "fop"} (io/resource "fop.xsd"))
+
   (def message-schema (m/schema (xds->registry {:default-ns "script"} (io/resource "V20170715/transport.xsd"))
                                 {:registry (merge
                                              (m/default-schemas)
