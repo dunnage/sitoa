a881769640c956757ea215f8f72e69ed9b0a284d
Tyler Tallman <dspiteself@gmail.com>
2022-11-15 22:53:04 -0600

    add edi parsing

--- edi-serde/src/com/breezeehr/edi_serde.clj
+++ edi-serde/src/com/breezeehr/edi_serde.clj
@@ -1,6 +1,26 @@
-(ns com.breezeehr.edi-serde)
+(ns com.breezeehr.edi-serde
+  (:require [clojure.java.io :as io])
+  (:import (io.xlate.edi.stream EDIInputFactory)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (prn x "Hello, World!"))
+
+
+(comment
+  (def fact  (EDIInputFactory/newFactory))
+  (.setProperty fact EDIInputFactory/EDI_IGNORE_EXTRANEOUS_CHARACTERS true)
+  (.setProperty fact EDIInputFactory/EDI_VALIDATE_CONTROL_STRUCTURE false)
+  (let [r (.createEDIStreamReader fact (io/input-stream (io/resource  "271.edi"
+                                                          #_"sample837-original.edi")))]
+    (loop []
+      (if (.hasNext r)
+        (let [next (.next r)]
+          (when (.isError next)
+            (prn (.getErrorType r)))
+          (prn (str (.getLocation r)) (str next))
+          (recur ))
+        ))
+    )
+  )
