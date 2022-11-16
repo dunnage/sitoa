edf16fb2f824ab901ce848ac1a138f82433206dd
Tyler Tallman <dspiteself@gmail.com>
2022-11-15 20:17:48 -0600

    add edi project to sitoa

---
+++ edi-serde/test/com/breezeehr/edi_serde_test.clj
@@ -0,0 +1,7 @@
+(ns com.breezeehr.edi-serde-test
+  (:require [clojure.test :refer :all]
+            [com.breezeehr.edi-serde :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
