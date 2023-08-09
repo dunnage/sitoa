c792ffc7aadb69528ad222ac7a434d79c4a1d77e
Tyler Tallman <dspiteself@gmail.com>
2023-08-09 16:01:45 -0500

    sitoa: clean up reflection warnings

--- serde/src/dunnage/sitoa/schema.clj
+++ serde/src/dunnage/sitoa/schema.clj
@@ -2,15 +2,14 @@
   (:require [clojure.java.io :as io])
   (:import (java.io Reader StringReader)
            (javax.xml.transform.stream StreamSource)
-           (javax.xml.validation SchemaFactory)))
-
+           (javax.xml.validation Schema SchemaFactory)))
 (defn schema-validator [schema]
-  (let [schemafactory (SchemaFactory/newDefaultInstance)
-        schema  (.newSchema schemafactory schema)
+  (let [schemafactory ^SchemaFactory (SchemaFactory/newDefaultInstance)
+        schema ^Schema (.newSchema schemafactory schema)
         validator (.newValidator schema)]
     (fn [data]
       (cond
-        (instance? Reader data) (.validate validator (StreamSource. data))
+        (instance? Reader data) (.validate validator (StreamSource. ^Reader data))
         (string? data)
         (.validate validator (StreamSource. (StringReader. data)))))))
 
