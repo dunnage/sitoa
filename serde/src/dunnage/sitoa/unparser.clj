b63cbb0272da4fe4c48d36502153a30cf90f537d
dspiteself <dspiteself@gmail.com>
2026-05-13 16:32:38 -0500

    constrain assert to not in seq-ex

--- serde/src/dunnage/sitoa/unparser.clj
+++ serde/src/dunnage/sitoa/unparser.clj
@@ -227,7 +227,7 @@
                               :exhausted true
                               :descrim pos
                               :item-data (nth data pos))
-                    (assert false (nth data pos))
+                    (assert in-regex? (nth data pos))
                     pos))
               pos)
             ))]
@@ -539,7 +539,7 @@
                         (recur pos (rest subparsers))
                         (do (assert (> pos ogpos) (pr-str data sch))
                             pos))))
-                  (do (assert false (pr-str [pos (drop pos data)]))
+                  (do (assert in-regex? (pr-str [pos (drop pos data)]))
                       pos))
                 pos)
               ))]
